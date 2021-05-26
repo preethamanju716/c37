6f72a5a8b40bc9a5990ef91f80a4b786be3d2293
preethamanju716 <73938378+preethamanju716@users.noreply.github.com>
2021-05-26 13:34:33 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,33 @@
+class Form {
+
+  constructor() {
+    //create input,button and greeting
+  }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+  }
+
+  display(){
+    var title = createElement('h2')
+    title.html("Car Racing Game");
+    title.position(130, 0);
+
+    input.position(130, 160);
+    button.position(250, 200);
+
+    button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(130, 100);
+    });
+
+  }
+}
---
+++ Player.js
@@ -0,0 +1,29 @@
+class Player {
+  constructor(){
+    //create index,distance,name
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "player" + playerCount;
+    database.ref(playerIndex).set({
+      name:name
+    });
+  }
+
+  static getPlayerInfo(){
+    
+  }
+}
